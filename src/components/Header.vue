0a300da9e428cec9db90e04b426b43321d0bc6fe
Jackie20222 <604673138@qq.com>
2023-06-18 16:53:35 +0800

    修改

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>header</header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+   
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
